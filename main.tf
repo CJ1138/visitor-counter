fa51ffd9b2e8531c0a240a7eac7161ce01173ec8
CJ1138 <chrisjohnson1138@gmail.com>
2022-10-07 12:26:27 +0100

    Started building out Terraform config and GH Actions wf.

---
+++ main.tf
@@ -0,0 +1,155 @@
+#Setup GCP Provider
+provider "google" {
+  project = "visitor-counter-qa"
+  region  = "europe-west2"
+}
+
+provider "google-beta" {
+  credentials = "visitor-counter-qa-1c59151f64a6.json"
+  project = "visitor-counter-qa"
+  region  = "europe-west2"
+}
+
+#Enable required APIs
+resource "google_project_service" "serviceusage" {
+  project                    = "visitor-counter-qa"
+  service                    = "serviceusage.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "datastore" {
+  project = "visitor-counter-qa"
+  service = "datastore.googleapis.com"
+}
+
+resource "google_project_service" "artifact_registry" {
+  project = "visitor-counter-qa"
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_project_service" "api_gateway" {
+  project = "visitor-counter-qa"
+  service = "apigateway.googleapis.com"
+}
+
+resource "google_project_service" "cloud_asset" {
+  project = "visitor-counter-qa"
+  service = "cloudasset.googleapis.com"
+}
+
+resource "google_project_service" "service_control" {
+  project = "visitor-counter-qa"
+  service = "servicecontrol.googleapis.com"
+}
+
+#Setup API Gateway
+resource "google_api_gateway_api" "vc_api" {
+  provider = google-beta
+  api_id   = "visitor-counter-api"
+}
+
+resource "google_api_gateway_api_config" "counter_gw" {
+  provider      = google-beta
+  api           = google_api_gateway_api.vc_api.api_id
+  api_config_id = "config"
+
+  openapi_documents {
+    document {
+      path     = "openapi.yaml"
+      contents = filebase64("openapi.yaml")
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_api_gateway_gateway" "api_gw" {
+  provider   = google-beta
+  api_config = "projects/300165146813/locations/global/apis/visitor-counter-api/configs/config"
+  gateway_id = "vc-api-gw"
+}
+
+resource "google_project_service" "enable_vc_api" {
+  project = "visitor-counter-qa"
+  service = "visitor-counter-0zrx04ynqpirh.apigateway.visitor-counter-api.cloud.goog"
+}
+
+/*resource "google_apikeys_key" "vc_api_key" {
+  provider   = google-beta
+  name         = "vc-api-key"
+  display_name = "Key for the Visitor Counter API"
+
+  restrictions {
+        api_targets {
+            service = "visitor-counter-0zrx04ynqpirh.apigateway.visitor-counter-api.cloud.goog"
+        }
+  }
+}*/
+
+##Workload Identity Federation
+
+resource "google_iam_workload_identity_pool" "counter-wi-pool" {
+  workload_identity_pool_id = "counter-wi-pool"
+  display_name              = "Visitor Counter WI Pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_wi_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.counter-wi-pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  display_name                       = "GitHub Actions"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.repository" = "assertion.repository"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account_iam_binding" "service-account-iam" {
+  service_account_id = "projects/visitor-counter-qa/serviceAccounts/github-actions-runner@visitor-counter-qa.iam.gserviceaccount.com"
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    "principalSet://iam.googleapis.com/projects/300165146813/locations/global/workloadIdentityPools/counter-wi-pool/attribute.repository/CJ1138/crc"
+  ]
+  depends_on = [
+    google_service_account.gh_actions_account
+  ]
+}
+
+#Create service accounts and assign roles
+resource "google_service_account" "gh_actions_account" {
+  account_id   = "github-actions-runner"
+  display_name = "GitHub Actions Runner"
+}
+
+resource "google_service_account" "tf_service_account" {
+  account_id   = "terraform-service-account"
+  display_name = "Terraform Service Account"
+}
+
+resource "google_project_iam_member" "tf_editor" {
+  project = "visitor-counter-qa"
+  role    = "roles/editor"
+  member  = "serviceAccount:terraform-service-account@visitor-counter-qa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "cloud_asset_owner" {
+  project = "visitor-counter-qa"
+  role    = "roles/cloudasset.owner"
+  member  = "serviceAccount:github-actions-runner@visitor-counter-qa.iam.gserviceaccount.com"
+}
+
+# Adding Terraform remote bucket
+
+resource "google_storage_bucket" "default" {
+  name          = "vc-qa-7968b717b2a4-bucket-tfstate"
+  force_destroy = false
+  location      = "EU"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
